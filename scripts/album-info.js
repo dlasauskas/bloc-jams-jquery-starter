8d919c356638ac82bd21094dabed2b8391a811f9
danielle.lasauskas <daniellelasauskas@gmail.com>
2019-04-11 20:59:01 -0500

    two

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,5 +1,6 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
-  $('#artist').text(artist.title);
-}
+  $('#artist').text(artist.main);
+  $('#release-info').text(release-info.main);
+};
